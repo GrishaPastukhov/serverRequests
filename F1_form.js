0f873bf2f1def3fc3c1b23e3fd39065758f6f098
GrishaPastukhov <grishafox505@gmail.com>
2023-03-30 15:08:21 +0300

    form done

--- F1_form.js
+++ F1_form.js
@@ -1,11 +1,15 @@
 'use strict'
 const submitBtn = document.querySelector('.requestForm__submitBtn');
 const txtArea = document.querySelector('.requestForm__txt');
-
+const disabledOff = () => {
+    submitBtn.removeAttribute('disabled')
+}
 submitBtn.addEventListener('click', () => {
     const options = {method: 'GET'};
+    submitBtn.setAttribute('disabled','');
 fetch(`https://jsonplaceholder.typicode.com/${txtArea.value}`)
   .then(response => response.json())
   .then(response => console.log(response))
-  .catch(err => console.error(err));
+  .catch(err => alert('Wrong address!'));
+  setTimeout(disabledOff, 500);
 })
